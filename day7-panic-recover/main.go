e79e761fb8e230b9bd49b423bd1a73420a2a609c
zhouxing9454 <zhouxing9454@gmail.com>
2023-03-19 02:11:43 +0800

    dev-2023-03-18-Panic

---
+++ day7-panic-recover/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"Gee/day7-panic-recover/gee"
+	"fmt"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
+func main() {
+	r := gee.Default()
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+	r.GET("/", func(context *gee.Context) {
+		context.HTML(http.StatusOK, "css.tmpl", nil)
+	})
+	r.GET("/students", func(context *gee.Context) {
+		context.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2}, //装了2个*student类型的数组变量.
+		})
+	})
+	r.GET("/date", func(context *gee.Context) {
+		context.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
+		})
+	})
+
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
+	})
+
+	r.Run(":9999")
+}
